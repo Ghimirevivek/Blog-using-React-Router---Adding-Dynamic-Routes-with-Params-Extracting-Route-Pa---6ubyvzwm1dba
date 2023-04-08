4c384ba5a12e77696bc70a62754139ffe8ace3fc
Ghimirevivek <stephengmre@gmail.com>
2023-04-08 14:41:48 +0530

    me

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 
 export default function PostDetail({ match }) {
   // Complete the function
+  return <h1>Post Details :{match.params.id}</h1>
 }
-  
